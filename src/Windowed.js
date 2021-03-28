656dd134706dc84b464099a31d9d5250f157e120
yaman0 <yaman0@protonmail.com>
2021-03-28 18:09:50 +0200

    :sparkles: user should hide window

--- src/Windowed.js
+++ src/Windowed.js
@@ -38,37 +38,55 @@ export default class Windowed extends HTMLElement{
         this.shadow.innerHTML = `
             <style>
                 .window-root {
+                    --window-primary-color: #d0d0d0;
+
                     background-color: white;
-                    border: solid 1px black;
-                    padding: 30px 60px;
+                    border: solid 1px var(--window-primary-color);
+                    min-width: 100px;
+                    max-width: 50%  ;
                     position: absolute;
                     z-index: 3
+
                 }
                 .window-sidebar {
-                    height: 20px;
-                    background-color: red;
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    right: 0;
-                    z-index: 1000
+                    display: flex;
+                    justify-content: space-between;
+                    padding: 0 5px;
+                    background-color: var(--window-primary-color);
+     
+                    z-index: 1000;
+                    font-size: 25px;
+                    user-select: none;
+                }
+                .window-body {
+                    padding: 3px 5px;
+                }
+                .window-tools>span:hover {
+                    cursor: pointer;
                 }
             </style>
             <div class="window-root">
                 <div class="window-sidebar">
-
+                    <div class="window-title">title</div>
+                    <div class="window-tools"><span class="window-hide">_</span></div>
                 </div>
-                <slot></slot>
+                <slot class="window-body"></slot>
             </div>
         `
 
         this.$root = this.shadow.querySelector('.window-root') 
         this.$sidebar = this.shadow.querySelector('.window-sidebar')
+        this.$hideButton = this.shadow.querySelector('.window-hide')
         this.$sidebar.addEventListener('click', ()=>{console.log('test')})
+        this.$body = this.shadow.querySelector('.window-body')
 
         this.attachDragEventOn(this.$sidebar)
+        this.attachHidenEvent()
+    }
 
-
+    attachHidenEvent() {
+        this.$hideButton.addEventListener('click', this.hide.bind(this))
+        this.renderHide()
     }
 
     /**
@@ -78,16 +96,15 @@ export default class Windowed extends HTMLElement{
     attachDragEventOn($element = null) {
         $element = $element ?? this.$root
         this.renderMove()
-        
-
-        $element.addEventListener('mouseup', (event)=>{
-            this.cursorPoint = null
-            document.removeEventListener('mousemove', this.onDragCallback)
-        })
 
         $element.addEventListener('mousedown', (event)=>{
             this.cursorPoint = new AppPoint(event.clientX, event.clientY)
             this.cursorPoint = this.cursorPoint.minus(this.pos)
+
+            document.addEventListener('mouseup', (event)=>{
+                this.cursorPoint = null
+                document.removeEventListener('mousemove', this.onDragCallback)
+            })
             document.addEventListener('mousemove', this.onDragCallback)
         })
     }
@@ -100,10 +117,18 @@ export default class Windowed extends HTMLElement{
             this.$root.style.left = this.pos.x + 'px'
     }
 
+    renderHide() {
+        let actualWidht = this.$root.offsetWidth
+        this.$hideButton.textContent = this.hiden ? '▭' : '-'
+        this.$root.style.width = this.hiden ? actualWidht+'px' : 'inherit'
+        this.$body.style.display = this.hiden ? 'none' : 'inherit'
+    }
+
     /**
      * init attributes
      */
     initAttributes() {
+        this.hiden = false
         this.pos = new AppPoint(this.getAttribute('left') ?? 0, this.getAttribute('top') ?? 0)
     }
 
@@ -115,5 +140,14 @@ export default class Windowed extends HTMLElement{
         this.pos = (new AppPoint(event.clientX, event.clientY)).minus(this.cursorPoint)
         this.renderMove()
         event.stopImmediatePropagation()
+    }
+
+    /**
+     * 
+     * @param {Event} event 
+     */
+    hide(event) {
+        this.hiden = !this.hiden
+        this.renderHide()
     }
 }
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Windowed from "./Windowed"
 import '../assets/style.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 customElements.define('app-windowed', Windowed)
 // document.addEventListener('click', (event) => {
 
