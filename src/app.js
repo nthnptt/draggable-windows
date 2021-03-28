dc78595d5874f522d2ac3b92f02c76de150873eb
yaman0 <yaman0@protonmail.com>
2021-03-28 19:41:46 +0200

    first try

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 import Windowed from "./Windowed"
 import '../assets/style.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-customElements.define('app-windowed', Windowed)
-// document.addEventListener('click', (event) => {
 
-// console.log(event)
-//     console.table({x: event.screenX, y: event.screenY})
-//     console.table({x: event.clientY, y: event.clientY})
-// })
+customElements.define('app-windowed', Windowed)
